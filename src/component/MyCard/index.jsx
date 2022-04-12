4a900ddb02d6fba927257602b0532b4727d038e4
udexmickey <jerrymike80@yahoo.com>
2022-04-12 01:52:48 +0100

    update

--- src/component/MyCard/index.jsx
+++ src/component/MyCard/index.jsx
@@ -4,7 +4,7 @@ import { Button, Card } from 'react-bootstrap'
 export default function MyCard(props) {
   return (
     <div className='myCard'>
-        <Card className='mb-3' style={{ color: '#fff', background: '#0D0D0D'}}>
+        <Card className='mb-3' style={{ color: 'var(--primary)', background: 'var(--background)'}}>
             <Card.Img src={props.img} />
             <Card.Body  style={{padding: '1rem 0px 0rem'}}>
                 <Card.Title  style={{ margin: '.1rem'}}>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import "./_variables.scss";
 import App from './App';
 
 ReactDOM.render(
