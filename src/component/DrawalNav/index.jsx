8e4ebcdcbb12f303534f54b2bc2e421a75c3eb83
udexmickey <jerrymike80@yahoo.com>
2022-08-04 04:10:51 +0100

    add logo image on dark mode and light mode

--- src/component/DrawalNav/index.jsx
+++ src/component/DrawalNav/index.jsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Collapse, Nav } from "react-bootstrap";
 import {BiMenu} from 'react-icons/bi'
 import { MdClose } from 'react-icons/md'
 import { BsBroadcast, BsDropletFill, BsFillChatLeftQuoteFill, BsFillPeopleFill, BsGearWideConnected, BsGridFill } from "react-icons/bs";
 import { FaFacebookF, FaHome, FaWallet } from "react-icons/fa";
 import { FiTwitter } from "react-icons/fi";
-import JOYSTICK from '../../assets/images/JOYSTICK-logo2.png'
+import JOYSTICK from '../../assets/images/JOYSTICK-logo.png'
+import JOYSTICK2 from '../../assets/images/JOYSTICK-light.png'
 import './drawalNav.scss'
 import ThemeToggle from "../ThemeToggle";
 import { Link, useLocation } from "react-router-dom";
@@ -14,16 +15,23 @@ export default function DrawalNav({TogglecloseOpen}) {
     const [open, setOpen] = useState(true);
 
     const {pathname} = useLocation();
-    // console.log(pathname);
-    // if(pathname === '/store'){
-    //   document.setAttribute('class', 'active')
+    let [themeData, setThemeData] = useState(localStorage.getItem('theme-dark'))
+    
+    useEffect(() => {
+          setThemeData(themeData)
+          console.log('mike');
+    }, [themeData])
 
-    // }
     return (
       <div className="" style={{position: 'relative'}}>
         <div className="side-nav">
           <div className="logo-title">
-            <img loading='eager' src={JOYSTICK} alt="JOYSTICK-logo" />
+            {/* <img loading='eager' src={JOYSTICK} alt="JOYSTICK-logo" /> */}
+
+            <picture>
+              <source srcSet={JOYSTICK}  media={`(prefers-color-scheme: ${themeData})`}/>
+              <img src={JOYSTICK2} alt="JOYSTICK-logo" />
+            </picture>
           </div>
           <p
             onClick={() => setOpen(!open)}
@@ -37,19 +45,19 @@ export default function DrawalNav({TogglecloseOpen}) {
           <div>
             <Collapse in={open} dimension="width">
               <div>
-                  <Nav className=" flex-column" variant="tabs" defaultActiveKey={pathname}>
+                  <Nav className=" flex-column" variant="tabs" defaultActiveKey={pathname.toString()}>
                     <div className="footer-nav">
-                      <Link to="/" className={ (pathname === '/' ?? pathname === '/home') && `active`}> <FaHome /> <span>Home</span></Link>
-                      <Link to="/store" className={ pathname === '/store' && `active`}> <BsDropletFill /> <span>Store</span></Link>
-                      <Link to="/library" className={ pathname === '/library' && `active`}> <BsGridFill/> <span>Library</span></Link>
-                      <Link to="/socials" className={ pathname === '/socials' && `active`}> <BsFillPeopleFill /> <span>Socials</span></Link>
-                      <Link to="/livestream" className={ pathname === '/livestream' && `active`}> <BsBroadcast /> <span>Live</span></Link>
+                      <Link to="/" className={ (pathname.toString() === '/' ?? pathname.toString() === '/home') && `active`}> <FaHome /> <span>Home</span></Link>
+                      <Link to="/store" className={ pathname.toString() === '/store' && `active`}> <BsDropletFill /> <span>Store</span></Link>
+                      <Link to="/library" className={ pathname.toString() === '/library' && `active`}> <BsGridFill/> <span>Library</span></Link>
+                      <Link to="/socials" className={ pathname.toString() === '/socials' && `active`}> <BsFillPeopleFill /> <span>Socials</span></Link>
+                      <Link to="/livestream" className={ pathname.toString() === '/livestream' && `active`}> <BsBroadcast /> <span>Live</span></Link>
                     </div>
 
                       <hr />
                       <div className="footer-nav-sids">
-                        <Link to="/u/wallet" className={ pathname === '/u/wallet' && `active`}><FaWallet /> <span>Wallet</span></Link>
-                        <Link to="/u/settings" className={ pathname === '/u/settings' && `active`}><BsGearWideConnected /> <span>Settings</span></Link>
+                        <Link to="/u/wallet" className={ pathname.toString() === '/u/wallet' && `active`}><FaWallet /> <span>Wallet</span></Link>
+                        <Link to="/u/settings" className={ pathname.toString() === '/u/settings' && `active`}><BsGearWideConnected /> <span>Settings</span></Link>
                         <div className="icons-sidenav-footer">
                             <Link to="#action2"><FaFacebookF /> </Link>
                             <Link to="#action3"><FiTwitter /> </Link>
