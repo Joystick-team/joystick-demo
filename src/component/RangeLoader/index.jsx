1cf97d517c52ef0d354561d8f4d75f9e3f57388d
udexmickey <jerrymike80@yahoo.com>
2022-08-15 17:16:30 +0100

    add max-min props RangeLoader component

--- src/component/RangeLoader/index.jsx
+++ src/component/RangeLoader/index.jsx
@@ -3,12 +3,12 @@ import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
 export default function RangeLoader(props) {
-    const value = 60.66;
+    // const value = 60.66;
 
   return (
     <div>
         <div className="range__loader" >
-            <CircularProgressbar value={props.value} text={`${props.value}%`} />
+            <CircularProgressbar value={props.value} maxValue={props.max} minValue={props.min} text={`${props.value}%`} />
             <div>{`${props.value}%`}</div>
         </div>
     </div>
