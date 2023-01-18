fdd1df304006a315301e6ecdd57dc5c2b5d1518a
dnor-dev <anorue4rss@hotmail.com>
2023-01-18 17:08:33 +0100

    chore: hooked notification feature with api

--- src/Actions/Notifications.action.js
+++ src/Actions/Notifications.action.js
@@ -1,23 +1,26 @@
 import { FETCH_NOTIFICATIONS } from "../Action-Creators/Notifications.AC";
 import axios from "axios";
 
-export const fetchAllNotifications = (page) => async (dispatch, getState) => {
-  try {
-    const { signin } = getState();
-    const userToken = signin?.userToken;
-    const config = {
-      headers: {
-        "Content-Type": "multipart/form-data",
-        Authorization: `Bearer ${userToken?.access_token}`,
-      },
-    };
-    const { data } = await axios.get(
-      `https://api.joysticklabs.io/api/v1/notification?page=${page}`,
-      config
-    );
-    console.log("notifications", data);
-    dispatch({ type: FETCH_NOTIFICATIONS, payload: data });
-  } catch (error) {
-    console.log("notifications error", error?.response?.data?.message);
-  }
-};
+export const fetchAllNotifications =
+  (page, setLoading) => async (dispatch, getState) => {
+    setLoading(true);
+    try {
+      const { signin } = getState();
+      const userToken = signin?.userToken;
+      const config = {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${userToken?.access_token}`,
+        },
+      };
+      const { data } = await axios.get(
+        `https://api.joysticklabs.io/api/v1/notification?page=${page}`,
+        config
+      );
+      dispatch({ type: FETCH_NOTIFICATIONS, payload: data });
+      setLoading && setLoading(false);
+    } catch (error) {
+      setLoading && setLoading(false);
+      console.log("notifications error", error?.response?.data?.message);
+    }
+  };
--- src/component/NotificationCard/index.jsx
+++ src/component/NotificationCard/index.jsx
@@ -1,39 +1,27 @@
 import React from "react";
 import { templates } from "./templates";
+import moment from "moment/moment";
 import "./card.scss";
 
-const NotificationCard = ({ title }) => {
-  const message =
-    title === "follow"
-      ? "Brooklyn Simmons followed you"
-      : title === "comments"
-      ? "James commented on your photo"
-      : title === "shares"
-      ? "Emmanuel shared your post"
-      : title === "likes"
-      ? "Ronald Richards liked your photo"
-      : title === "photos"
-      ? "Devon Lane added a new photo"
-      : "You have new notifications";
-
+const NotificationCard = ({ notification }) => {
   return (
     <div className="notification-card">
       <div className="notify-body">
-        <div className={templates[title].className}>
-          {templates[title].icon}
+        <div className={templates[notification.type].className}>
+          {templates[notification.type].icon}
         </div>
         <div className="notify-contents">
           <div className="profile-pics">
             <img src="/assets/images/joystick-user.png" alt="profile-pic" />
           </div>
           <div className="notify-details">
-            <p>{message}</p>
-            <p>5 mins ago</p>
+            <p>{notification.message}</p>
+            <p>{moment(notification.created_at).fromNow()}</p>
           </div>
         </div>
       </div>
 
-      {templates[title].follow_btn && (
+      {templates[notification.type].follow_btn && (
         <div className="follow-btn">
           <button className="follow">Follow</button>
         </div>
