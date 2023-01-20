5665412c7cd7a3de82ab30bc0871e73e21ad1a03
dnor-dev <anorue4rss@hotmail.com>
2023-01-21 00:19:38 +0100

    bug: fixed notification module

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import './App.scss'
-import Leaderboard from './component/ContentTab/Leaderboard'
-import Message from './component/ContentTab/Message'
-import Rewards from './component/ContentTab/Rewards'
-import Staking from './component/ContentTab/Staking'
-import AppContextContainer from './context/AppContext'
-import Layout from './Layout'
-import ErrorBoundary from './Layout/ErrorBoundary'
-import ErrorPage from './pages/ErrorPage'
-import HomePage from './pages/Home'
-import LandingPage from './pages/LandingPage'
-import Library from './pages/Library'
-import Livescream from './pages/Live'
-import WebinarCall from './pages/Live/WebinarCall'
-import Settings from './pages/Settings'
-import Socials from './pages/Socials'
-import Store from './pages/Store'
-import TestPage from './pages/TestPage'
-import Wallet from './pages/Wallet'
-import { ThemeProvider } from './ThemeContext'
-import NewSocials from './pages/NewSocials';
-import Feeds from './pages/NewSocials/pages/Feeds';
-import Profile from './pages/NewSocials/pages/Profile'
-import ProfileContents from './pages/NewSocials/pages/Profile/ProfileContents'
-import Posts from './pages/NewSocials/pages/Profile/ProfileContents/posts/posts'
-import Streams from './pages/NewSocials/pages/Profile/ProfileContents/streams/streams'
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.scss";
+import Leaderboard from "./component/ContentTab/Leaderboard";
+import Message from "./component/ContentTab/Message";
+import Rewards from "./component/ContentTab/Rewards";
+import Staking from "./component/ContentTab/Staking";
+import AppContextContainer from "./context/AppContext";
+import Layout from "./Layout";
+import ErrorBoundary from "./Layout/ErrorBoundary";
+import ErrorPage from "./pages/ErrorPage";
+import HomePage from "./pages/Home";
+import LandingPage from "./pages/LandingPage";
+import Library from "./pages/Library";
+import Livescream from "./pages/Live";
+import WebinarCall from "./pages/Live/WebinarCall";
+import Settings from "./pages/Settings";
+import Socials from "./pages/Socials";
+import Store from "./pages/Store";
+import TestPage from "./pages/TestPage";
+import Wallet from "./pages/Wallet";
+import { ThemeProvider } from "./ThemeContext";
+import NewSocials from "./pages/NewSocials";
+import Feeds from "./pages/NewSocials/pages/Feeds";
+import Profile from "./pages/NewSocials/pages/Profile";
+import ProfileContents from "./pages/NewSocials/pages/Profile/ProfileContents";
+import Posts from "./pages/NewSocials/pages/Profile/ProfileContents/posts/posts";
+import Streams from "./pages/NewSocials/pages/Profile/ProfileContents/streams/streams";
+import Notifications from "./pages/Notifications";
 
 function App() {
   return (
@@ -37,11 +37,16 @@ function App() {
             <ThemeProvider>
               <Layout>
                 <Routes>
-                  <Route exact path='/' element={<LandingPage />} />
-                  <Route exact path='/page/:id' element={<LandingPage />} />
-                  <Route exact path='/home' element={<HomePage />} />
+                  <Route exact path="/" element={<LandingPage />} />
+                  <Route exact path="/page/:id" element={<LandingPage />} />
+                  <Route exact path="/home" element={<HomePage />} />
                   {/* <Route path='/socials' element={<Socials />} /> */}
-                  <Route path='/test' element={<TestPage />} />
+                  <Route path="/test" element={<TestPage />} />
+                  <Route
+                    exact
+                    path="/notifications"
+                    element={<Notifications />}
+                  />
 
                   <Route path="/store" element={<Store />} />
                   <Route path="/livestream" element={<Livescream />} />
@@ -69,41 +74,17 @@ function App() {
                   <Route exact path="/staking" element={<Staking />} />
                   <Route exact path="/rewards" element={<Rewards />} />
                   <Route exact path="/message" element={<Message />} />
-                  <Route
-                      exact
-                      path='/socials' element={<NewSocials />}
-                       >
-                   <Route
-                      exact
-                      path="" element={<Feeds />}
-                      />
+                  <Route exact path="/socials" element={<NewSocials />}>
+                    <Route exact path="" element={<Feeds />} />
 
-                     <Route
-                      exact
-                      path="profile" element={<Profile />}
-                      >
+                    <Route exact path="profile" element={<Profile />}>
+                      <Route exact element={<ProfileContents />}>
+                        <Route exact path="" element={<Posts />} />
 
-                     <Route
-                      exact
-                        element={<ProfileContents />}
-                      >
-
-                      <Route
-                      exact
-                      path="" element={<Posts />}
-                      />
-
-                      <Route
-                      exact
-                      path="streams" element={<Streams />}
-                      />
-
-                    
+                        <Route exact path="streams" element={<Streams />} />
+                      </Route>
                     </Route>
-                    </Route>
-
-
-                </ Route>
+                  </Route>
                 </Routes>
               </Layout>
             </ThemeProvider>
--- src/pages/Notifications/index.jsx
+++ src/pages/Notifications/index.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NotificationCard from "../../component/NotificationCard";
 import ActiveFriends from "../../component/ActiveFriends";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchAllNotifications } from "../../Actions/Notifications.Action";
+import NotificationsLoader from "../../component/NotificationsLoader";
 import "./notifications.scss";
 
 const Notifications = () => {
+  const { notifications } = useSelector((state) => state);
+  const { userToken } = useSelector((state) => state.signin);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    userToken?.access_token &&
+      dispatch(fetchAllNotifications(page, setLoading));
+  }, [page]);
+
   return (
     <div className="notifications">
       <div className="notifications-content">
@@ -11,11 +26,27 @@ const Notifications = () => {
           <div className="header">
             <p className="header-text">Notifications</p>
           </div>
-          <div className="left-body">
-            <NotificationCard title="follow" />
-            <NotificationCard title="shares" />
-            <NotificationCard title="photos" />
-          </div>
+          {loading ? (
+            <NotificationsLoader />
+          ) : notifications?.data?.length !== 0 ? (
+            <div className="left-body">
+              {notifications?.data?.map((notification) => (
+                <NotificationCard notification={notification} />
+              ))}
+            </div>
+          ) : (
+            <div className="empty-data">
+              You don't have any notifications yet.
+            </div>
+          )}
+
+          {notifications?.data?.length !== 0 && (
+            <div className="load-more-btn">
+              <button disabled={loading} onClick={() => setPage(page + 1)}>
+                Load More..
+              </button>
+            </div>
+          )}
         </div>
 
         <div className="right-section">
